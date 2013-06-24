d6ee8618e4c824a44f0364970664a3cf720e945d
Sanky <gsanky@gmail.com>
2013-06-24 18:42:13 +0200

    i accidentally made a vwf

--- main.asm
+++ main.asm
@@ -216,9 +216,10 @@ WriteChar: ; 1f96
 	rst $8 ; 1
 	nop
 	nop
+	nop
 	
 	;ei
-	inc hl
+	;inc hl
 	ld a, h
 	ld [$c6c2], a
 	ld a, l
@@ -263,7 +264,7 @@ INCBIN "baserom.gbc", $1c000,$27a0
 
 ; free space
 
-VWFont:
+VWFFont:
 INCBIN "gfx/vwffont.1bpp"
 
 VWFTable:
@@ -303,12 +304,287 @@ HackPredef:
 
 WriteCharAdvice:
     ld a, [hSaveA]
+    call WriteVWFChar
     ; original code
-	di ; 1
-	call $17cb ; 3
-	ld [hl], a 
-	ei
+	;di ; 1
+	;call $17cb ; 3
+	;ld [hl], a 
+	;ei
     ret
+
+CopyColumn:
+    ; b = source column
+    ; c = dest column
+    ; de = source number
+    ; hl = dest number
+    push hl
+    push de
+    ld a, $08
+    ;ld [VWFCurTileRow], a
+.Copy
+    push af
+    ld a, [de]
+    and a, b
+    jr nz, .CopyOne
+.CopyZero
+    ld a, %11111111
+    xor c
+    and [hl]
+    jp .Next
+.CopyOne
+    ld a, c
+    or [hl]
+.Next
+    ld [hli],a
+    inc de
+    ;ld a, [VWFCurTileRow]
+    pop af
+    dec a
+    ;ld [VWFCurTileRow], a
+    jp nz, .Copy
+    pop de
+    pop hl
+    ret
+    
+WriteVWFChar:
+    push de
+    push hl
+    ld [VWFChar], a
+    ; Store the original tile location.
+    push hl
+    pop de
+    ld hl, VWFTileLoc
+    ld [hl], d
+    inc hl
+    ld [hl], e
+    
+    ; Check if VWF is enabled, bail if not.
+    ;ld a, [W_VWF_ENABLED]
+    ;dec a
+    
+    ; write to tilemap
+    pop hl
+    ld a, [VWFCurTileNum]
+    add $80
+    ;push af
+    di
+    call $17cb
+    ld [hl], a
+    ei
+    push hl
+    
+    ; Get the character in VWF's font.
+    ld a, [VWFChar]
+    cp $80
+    jr c, .high
+    sub a, $80
+    jr .gotchar
+.high
+    add a, $20
+.gotchar
+    ld [VWFChar], a
+    ; Store the character tile in BuildArea0.
+    ld hl, VWFFont
+    ld b, 0
+    ld c, a
+    ld a, $8
+    call AddNTimes
+    ld bc, $0008
+    ld de, VWFBuildArea0
+    call CopyBytes ; copy bc source bytes from hl to de
+    
+    ld a, $1
+    ld [VWFNumTilesUsed], a
+    
+    ; Get the character length from the width table.
+    ld a, [VWFChar]
+    ld c, a
+    ld b, $00
+    ld hl, VWFTable
+    add hl, bc
+    ld a, [hl]
+    ld [VWFCharWidth], a
+.WidthWritten
+    ; Set up some things for building the tile.
+    ; Special cased to fix column $0, which is invalid (not a power of 2)
+    ld de, VWFBuildArea0
+    ld hl, VWFBuildArea2
+    ;ld b, a
+    ld b, %10000000
+    ld a, [VWFCurTileCol]
+    and a
+    jr nz, .ColumnIsFine
+    ld a, $80
+.ColumnIsFine
+    ld c, a ; a
+.DoColumn
+    ; Copy the column.
+    call CopyColumn
+    rr c
+    jr c, .TileOverflow
+    rrc b
+    ld a, [VWFCharWidth]
+    dec a
+    ld [VWFCharWidth], a
+    jr nz, .DoColumn 
+    jr .Done
+.TileOverflow
+    ld c, $80
+    ld a, $2
+    ld [VWFNumTilesUsed], a
+    ld hl, VWFBuildArea3
+    jr .ShiftB
+.DoColumnTile2
+    call CopyColumn
+    rr c
+.ShiftB
+    rrc b
+    ld a, [VWFCharWidth]
+    dec a
+    ld [VWFCharWidth], a
+    jr nz, .DoColumnTile2
+.Done
+    ld a, c
+    ld [VWFCurTileCol], a
+    
+    ;ld de, W_VWF_BUILDAREA1
+    ;ld hl, W_VWF_BUILDAREA3
+    
+    
+    
+    ; 1bpp -> 2bpp
+    ;ld b, 0
+    ;ld c, $10
+    ;ld hl, VWFBuildArea2
+    ;;call DelayFrame
+    ;ld de, VWFCopyArea
+    ;call FarCopyBytesDouble ; copy bc*2 bytes from a:hl to de ; XXX don't far
+
+    ; Get the tileset offset.
+    ld hl, $8800 ; $8ba0
+    ld a, [VWFCurTileNum]
+    ld b, $0
+    ld c, a
+    ld a, 16
+    call AddNTimes
+    
+    push hl
+    pop de
+    
+    ; Write the new tile(s)
+    ; Let's try DMA instead!
+
+    ld hl, VWFBuildArea2
+    ld bc, $0008
+    call CopyVRAMDataDouble
+
+
+    ld a, [VWFNumTilesUsed]
+    dec a
+    dec a
+    jr nz, .SecondAreaUnused
+    
+    ; If we went over one tile, make sure we start with it next time.
+    ; also move through the tilemap.
+    ld a, [VWFCurTileNum]
+    inc a
+    ld [VWFCurTileNum], a
+    ld a, $00
+    ld hl, VWFBuildArea3
+    ld de, VWFBuildArea2
+    ld bc, $0008
+    call CopyBytes
+    ld hl, VWFBuildArea3
+    ld a, $0
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a ; lazy
+    
+    pop hl
+    inc hl
+    ld a, [VWFCurTileNum]
+    add $80
+    
+    di
+    call $17cb
+    ld [hl], a
+    ei
+    
+    push hl
+    jr .FixOverflow
+.SecondAreaUnused
+    ; stupid bugfix for when the char didn't overflow, but the next char starts on the next tile.
+    ;ld a, [VWFCurTileCol]
+    ;cp $1
+    ;jr nz, .FixOverflow
+    ;pop hl
+    ;inc hl
+    ;push hl
+.FixOverflow
+    ; If we went over the last character allocated for VWF tiles, wrap around.
+    ld a, [VWFCurTileNum]
+    cp $e1-$80 ; may need tweaking
+    jr c, .AlmostDone
+    ld a, $00
+    ld [VWFCurTileNum], a ; Prevent overflow
+.AlmostDone
+    ;call WaitDMA
+    pop hl
+    
+    ld a, h
+    ld [$c6c2], a
+    ld a, l
+    ld [$c6c3], a
+    
+    pop de
+    ret
+
+CopyBytes: ; 0x3026
+; copy bc bytes from hl to de
+	inc b  ; we bail the moment b hits 0, so include the last run
+	inc c  ; same thing; include last byte
+	jr .HandleLoop
+.CopyByte
+	ld a, [hli]
+	ld [de], a
+	inc de
+.HandleLoop
+	dec c
+	jr nz, .CopyByte
+	dec b
+	jr nz, .CopyByte
+	ret
+	
+AddNTimes: ; 0x30fe
+; adds bc n times where n = a
+	and a
+	ret z
+.loop
+	add hl, bc
+	dec a
+	jr nz, .loop
+	ret
+; 0x3105
+
+CopyVRAMDataDouble: ;  cb7, hl=from, de=to, bc=how many/2
+	ld a, [hli]
+	di
+	call $17cb
+	ld [de], a
+	inc de
+	ld [de], a
+	ei
+	inc de
+	dec bc
+	ld a, b
+	or c
+	jr nz, CopyVRAMDataDouble ; 0xcc2 $f3
+	ret
 
 SECTION "bank8",DATA,BANK[$8]
 INCBIN "baserom.gbc", $20000,$4000
