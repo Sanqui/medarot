eb1e3537e815c98bcb0bf657d07f3b39ef039ce6
Sanky <gsanky@gmail.com>
2013-07-22 14:01:49 +0200

    TextTableBanks and Offsets (and macros)

--- main.asm
+++ main.asm
@@ -155,10 +155,46 @@ PutChar:
 	cp $4a
 	jp z, $1f5f
 	jp WriteChar
-; 0x1d3b
 
+TextTableBanks: ; 0x1d3b
+    db $0c
+    db $0d
+    db $0e
+    db $0f
+    db $16
+    db $13
+    db $00
+    db $00
+    db $18
+    db $00
+    db $1a
+    db $00
+    db $00
+    db $1d
+    db $00
+    db $00
 
-INCBIN "baserom.gbc", $1d3b,$1f96-$1d3b
+TextTableOffsets: ; 0x1d4b
+    dw $7e00
+    dw $7e00
+    dw $7e00
+    dw $7e00
+    dw $6000
+    dw $7800
+    dw $4000
+    dw $4000
+    dw $4000
+    dw $4000
+    dw $4000
+    dw $4000
+    dw $4000
+    dw $4000
+    dw $4000
+    dw $4000
+
+;1d6b
+
+INCBIN "baserom.gbc", $1d6b,$1f96-$1d6b
 
 WriteChar: ; 1f96
 	ld a, [hl]
